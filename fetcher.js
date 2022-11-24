50a025fe4cb185c98cd4e2bc9b9df32a1f21e14b
Lyon <lyon.b.lee@gmail.com>
2022-11-24 16:28:57 -0500

    updated fetcher meeting all conditions

--- fetcher.js
+++ fetcher.js
@@ -7,8 +7,22 @@ const args = process.argv;
 
 
 request(args[2], (error, response, body) => {
-  if(error) {
+  //console.log(response);   // this is for http response code
+  if(response.statusCode !== 200 || error) {
     return console.log(`${args[2]} is not a valid website.`);
+  }
+  // console.log(response);
+
+  let arr = args[3].split('/').slice(0, -1);    //split changes string into an array based on /, then slice extracts the arr from initial to second last
+  let filename = args[3].split('/').slice(-1);
+  let pathway = arr.join('/');      // Because arr removed / from the initial string, we need to add it back
+
+  if(!fs.existsSync(pathway)) {
+    return console.log(`${args[3]} is an invalid pathway!`);
+  }
+
+  if(fs.existsSync(args[3])) {
+    return console.log(`${filename} already exists in ${pathway}! Please rename.`);
   }
 
   fs.writeFile(args[3], body, err => {
@@ -16,7 +30,8 @@ request(args[2], (error, response, body) => {
       console.error();
     }
     // file written successfully
-    console.log(`Downloaded from ${args[2]} and saved ${body.length} bytes at ${args[3]}`);
+    // In response, content-length was placed in quotes because it has special character (hyphen) in it
+    console.log(`Downloaded from ${args[2]} and saved ${response.headers['content-length']} bytes at ${args[3]}`);
   });
 });
 
